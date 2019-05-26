f8f380eb1a68d455b2046eb24bc7290e7cfedc4f
seast19 <seast19@qq.com>
2019-05-26 21:05:17 +0800

    add

---
+++ index.py
@@ -0,0 +1,118 @@
+import time
+
+import requests
+import threading
+import queue
+
+import shelve
+
+q = queue.Queue()
+
+q_write = queue.Queue()
+
+theading_max_num = 50
+theading_now_num = 0
+
+
+# 根据单词搜索结果
+def searchWord():
+    global theading_now_num
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
+        'Referer': 'https://fanyi.baidu.com/'
+    }
+    while not q.empty():
+        try:
+            text = q.get()
+            resp = requests.post('https://fanyi.baidu.com/sug', data={'kw': text}, headers=headers)
+            data = resp.json()['data']
+            if len(data) == 0:
+                continue
+            else:
+                q_write.put(resp.json()['data'])
+                print('--> %s <--搜索完成' % text)
+        except Exception as e:
+            print(e)
+
+    theading_now_num += 1
+    print('退出线程')
+
+
+# 从结果队列读取数据写入文本
+def writeResults():
+    global theading_now_num
+    global theading_max_num
+
+    with shelve.open('./data/data') as f:
+        while theading_now_num < theading_max_num:
+            text = q_write.get()
+            try:
+                key = text[0]['k']
+                f[key] = text
+                print('写入了%s' % key)
+            except Exception as e:
+                print(e)
+
+    print('写入文本结束')
+
+
+# 从文件获取单词添加到队列
+def putWords2queue():
+    with open('./data/words.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            # print(line.strip())
+
+            q.put(line.strip())
+            # break
+    print('单词加入搜索队列结束')
+    print('*' * 20)
+
+
+# 发布任务
+def productTasks():
+    global theading_max_num
+    # 从文件获取单词添加到队列
+    t1 = threading.Thread(target=putWords2queue)
+    time.sleep(1)
+    t1.start()
+
+    # 搜索结果
+    for i in range(theading_max_num):
+        t = threading.Thread(target=searchWord)  # 创建线程
+        t.start()
+
+    # 读取结果
+    t2 = threading.Thread(target=writeResults)
+    t2.start()
+    t2.join()
+
+
+# 根据单词搜索结果
+def readShelve(text):
+    # print('查询 %s 结果:' % text)
+    with shelve.open('./data/data') as f:
+        if text in f:
+            result = f.get(text)
+            print('共 %d 条数据:' % len(result))
+            # print('-' * 20)
+            for i, v in enumerate(result):
+                print('  [%d] %s' % (i + 1, v['v']))
+
+        else:
+            print('共 0 条数据')
+
+    print('*' * 20)
+
+
+if __name__ == '__main__':
+    print('欢迎使用离线英语词典！')
+    print('*'*30)
+
+    # 生产者
+    # productTasks()
+
+    # 开始查询
+    while True:
+        text = input('请输入英文单词：')
+        print('-' * 20)
+        readShelve(text)
---
+++ readme.md
@@ -0,0 +1,22 @@
+# 本地英语词典查询
+
+* 离线所有单词翻译结果只本地
+* 使用百度翻译接口
+* 使用[dwyl/english-words](https://github.com/dwyl/english-words)提供的词典
+
+
+**使用方法**
+
+* Windows
+    
+    下载release下的zip包，解压运行exe文件即可查询
+
+* linux
+
+    安装依赖后运行`index.py`
+    
+    ```    
+    sudo pip install -r requirements.txt
+    python3 index.py
+    ```
+    
